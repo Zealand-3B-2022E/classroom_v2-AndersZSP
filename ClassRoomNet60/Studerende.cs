f87934620b396491fd4f2181196cf47455455e22
Anders Zune Sander Petersen <ande939h@edu.easj.dk>
2022-09-05 13:30:08 +0200

    Studerende klasse properties og instance field

---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Studerende
+    {
+        private string _navn;
+        private int _fødselsår;
+        private int _fødselsmåned;
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+        public int Fødselsår
+        {
+            get { return _fødselsår; }
+            set { _fødselsår = value; }
+        }
+        public int Fødselsmåned
+        {
+            get { return _fødselsmåned; }
+            set
+            {
+                _fødselsmåned = value;
+            }
+        }
+
+        public Studerende(string navn, int fødselsår, int fødselsmåned)
+        {
+            _navn = navn;
+            _fødselsår = fødselsår;
+            _fødselsmåned = fødselsmåned;
+        }
+    }
+}
